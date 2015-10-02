06cd177ff522183bf32f7f12522a47e3748eaee6
Dan Meliza <dan@meliza.org>
2015-10-02 13:43:25 -0400

    tick version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ import sys
 
 # --- Distutils setup and metadata --------------------------------------------
 
-VERSION = '0.4.6-SNAPSHOT'
+VERSION = '0.4.6'
 
 cls_txt = """
 Development Status :: 5 - Production/Stable
