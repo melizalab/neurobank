407e99922a2d6d1ef6aa221aadc7a5f2f0df8c2d
Dan Meliza <dan@meliza.org>
2023-05-17 12:38:49 -0400

    replace requests with httpx in admin script; handle missing resources

--- nbank/admin.py
+++ nbank/admin.py
@@ -7,42 +7,62 @@ example, if a lot of files were deposited erroneously.
 """
 import logging
 from pathlib import Path
-import requests as rq
+import httpx
 from nbank import __version__, registry, archive, util
 from nbank.script import setup_log, userpwd
 
 log = logging.getLogger("nbank")  # root logger
 
 
+def delete_resource(
+    resource_id: str, *, session: httpx.Client, registry_url: str, dry_run: bool = False
+):
+    log.info("%s:", resource_id)
+    url, params = registry.get_locations(registry_url, resource_id)
+    for loc in util.query_registry_paginated(session, url, params):
+        partial = util.parse_location(loc)
+        if not isinstance(partial, Path):
+            continue
+        try:
+            path = archive.resolve_extension(partial)
+        except FileNotFoundError:
+            log.info("  - %s has already been deleted", partial)
+            continue
+        log.info("  ✗ deleted %s", path)
+        if not dry_run:
+            if path.is_dir():
+                path.rmdir()
+            else:
+                path.unlink()
+    url, params = registry.get_resource(args.registry_url, resource_id)
+    req = session.build_request("DELETE", url)
+    log.info("  ✗ purged %s", req.url)
+    if not dry_run:
+        r = session.send(req)
+        r.raise_for_status()
+
+
 def delete_resources(args):
     if args.dry_run:
         log.info("DRY RUN")
 
-    with open(args.resources, "rt") as fp, rq.Session() as session:
+    with open(args.resources, "rt") as fp, httpx.Client(auth=args.auth) as session:
         for line in fp:
             resource_id = line.strip()
             if len(resource_id) == 0 or resource_id.startswith("#"):
                 continue
-            log.info("%s:", resource_id)
-            log.info("- deleting from local archives:")
-            url, params = registry.get_locations(args.registry_url, resource_id)
-            for loc in util.query_registry_paginated(session, url, params):
-                partial = util.parse_location(loc)
-                if isinstance(partial, Path):
-                    path = archive.resolve_extension(partial)
-                    log.info("  ✗ %s", path)
-                    if not args.dry_run:
-                        if path.is_dir():
-                            path.rmdir()
-                        else:
-                            path.unlink()
-            log.info("- purging from registry:")
-            url, params = registry.get_resource(args.registry_url, resource_id)
-            req = rq.Request("DELETE", url)
-            prepped = session.prepare_request(req)
-            log.info("  ✗ %s", prepped.url)
-            if not args.dry_run:
-                session.send(prepped)
+            try:
+                delete_resource(
+                    resource_id,
+                    session=session,
+                    registry_url=args.registry_url,
+                    dry_run=args.dry_run,
+                )
+            except httpx.HTTPStatusError as err:
+                if err.response.status_code == 404:
+                    log.error("  - not in the registry, skipping")
+                else:
+                    raise err
 
 
 if __name__ == "__main__":
@@ -65,7 +85,7 @@ if __name__ == "__main__":
         help="username:password to authenticate with registry. "
         "If not supplied, will attempt to use .netrc file",
         type=userpwd,
-        default=None,
+        default=httpx.NetRCAuth(None),
     )
     sub = p.add_subparsers(title="subcommands")
 
