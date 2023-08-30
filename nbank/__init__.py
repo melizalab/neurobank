d1eedb1447732c734f33f5dddf573527e58f0e2f
Dan Meliza <dan@meliza.org>
2023-08-30 12:42:32 -0400

    bump version

--- nbank/__init__.py
+++ nbank/__init__.py
@@ -5,7 +5,7 @@
 Copyright (C) 2013--2023 Dan Meliza <dan@meliza.org>
 """
 
-__version__ = "0.10.6"
+__version__ = "0.10.7"
 
 from nbank.core import deposit, describe, fetch, find, get, search, verify
 
