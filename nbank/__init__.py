205b0f7678293b877797febbdc7cff128a2ab3a6
Dan Meliza <dan@meliza.org>
2024-12-20 15:40:52 -0500

    get version from pyproject.toml

--- nbank/__init__.py
+++ nbank/__init__.py
@@ -3,7 +3,13 @@
 
 Copyright (C) 2013--2024 Dan Meliza <dan@meliza.org>
 """
-__version__ = "0.10.10"
+try:
+    from importlib.metadata import version
+    __version__ = version("neurobank")
+except Exception:
+    # If package is not installed (e.g. during development)
+    __version__ = "unknown"
+
 
 
 # Variables:
