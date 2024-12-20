d88423b27802ba286d68b3ea5415fbc0fcafde64
Dan Meliza <dan@meliza.org>
2024-12-20 14:36:15 -0500

    bump version

--- nbank/__init__.py
+++ nbank/__init__.py
@@ -1,9 +1,9 @@
 # -*- mode: python -*-
 """neurobank data management system
 
-Copyright (C) 2013--2023 Dan Meliza <dan@meliza.org>
+Copyright (C) 2013--2024 Dan Meliza <dan@meliza.org>
 """
-__version__ = "0.10.9"
+__version__ = "0.10.10"
 
 
 # Variables:
